66a344008bfca07b75d090e3fc49c3b149a77c1b
kurt <s1788674@ed.ac.uk>
2018-06-03 17:37:47 +0100

    fix syntax

--- src/__init__.py
+++ src/__init__.py
@@ -3,62 +3,69 @@ import json
 import sys
 from os import path
 
-
 def load_config(path):
+    global config, cfg_data, cfg_dir, cfg_net, cfg_log,\
+           PRJDIR, SRCDIR, RESDIR, DATDIR, MDLDIR, WAVDIR,\
+           HTS1DIR, HTS2DIR, LAB1DIR, LAB2DIR, LAB3DIR,\
+           ACO1DIR, ACO2DIR, ACO3DIR, TXTDIR, STTDIR,\
+           TRNDIR, VALDIR, TSTDIR, SYNDIR,\
+           STDOUT, STDERR, VERBOSE
+
     with open(path) as f:
-        global config = json.load(f)
-    global cfg_data = config['data']
-    global cfg_dir = config['directories']
-    global cfg_net = config['network']
-    global cfg_log = config['log']
+        config = json.load(f)
+    cfg_data = config['data']
+    cfg_dir = config['directories']
+    cfg_net = config['network']
+    cfg_log = config['log']
 
-    global PRJDIR = path.dirname(path.dirname(path.realpath(sys.argv[0])))
-    global SRCDIR = path.join(PRJDIR, 'src')
-    global RESDIR = path.join(PRJDIR, 'res')
-    global DATDIR = path.join(PRJDIR, 'data') # for default values
-    global MDLDIR = cfg_dir.get('models', path.join(DATDIR, 'models'))\
-                    .replace('$', PRJDIR)
-    global WAVDIR = cfg_dir.get('sounds', path.join(DATDIR, 'wav'))\
-                    .replace('$', PRJDIR)
-    global HTS1DIR = cfg_dir.get('raw-states', path.join(DATDIR, 'hts1'))\
-                     .replace('$', PRJDIR)
-    global HTS2DIR = cfg_dir.get('realigned-states', path.join(DATDIR, 'hts2'))\
-                     .replace('$', PRJDIR)
-    global LAB1DIR = cfg_dir.get('raw-linguistics', path.join(DATDIR, 'lab1'))\
-                     .replace('$', PRJDIR)
-    global LAB2DIR = cfg_dir.get('trimmed-linguistics', path.join(DATDIR, 'lab2'))\
-                     .replace('$', PRJDIR)
-    global LAB3DIR = cfg_dir.get('normalized-linguistics', path.join(DATDIR, 'lab3'))\
-                     .replace('$', PRJDIR)
-    global ACO1DIR = cfg_dir.get('raw-acoustics', path.join(DATDIR, 'aco1'))\
-                     .replace('$', PRJDIR)
-    global ACO2DIR = cfg_dir.get('trimmed-acoustics', path.join(DATDIR, 'aco2'))\
-                     .replace('$', PRJDIR)
-    global ACO3DIR = cfg_dir.get('delta-acoustics', path.join(DATDIR, 'aco3'))\
-                     .replace('$', PRJDIR)
-    global TXTDIR = cfg_dir.get('transcripts', path.join(DATDIR, 'txt'))\
-                    .replace('$', PRJDIR)
-    global STTDIR = cfg_dir.get('statistics', path.join(DATDIR, 'stats'))\
-                    .replace('$', PRJDIR)
-    global TRNDIR = cfg_dir.get('training', path.join(DATDIR, 'training'))\
-                    .replace('$', PRJDIR)
-    global VALDIR = cfg_dir.get('validation', path.join(DATDIR, 'validation'))\
-                    .replace('$', PRJDIR)
-    global TSTDIR = cfg_dir.get('testing', path.join(DATDIR, 'training'))\
-                    .replace('$', PRJDIR)
-    global SYNDIR = cfg_dir.get('synthesized', path.join(DATDIR, 'synthesized'))\
-                    .replace('$', PRJDIR)
+    PRJDIR = path.dirname(path.dirname(path.realpath(sys.argv[0])))
+    SRCDIR = path.join(PRJDIR, 'src')
+    RESDIR = path.join(PRJDIR, 'res')
+    DATDIR = path.join(PRJDIR, 'data') # for default values
+    MDLDIR = cfg_dir.get('models', path.join(DATDIR, 'models'))\
+             .replace('$', PRJDIR)
+    WAVDIR = cfg_dir.get('sounds', path.join(DATDIR, 'wav'))\
+             .replace('$', PRJDIR)
+    HTS1DIR = cfg_dir.get('raw-states', path.join(DATDIR, 'hts1'))\
+              .replace('$', PRJDIR)
+    HTS2DIR = cfg_dir.get('realigned-states', path.join(DATDIR, 'hts2'))\
+              .replace('$', PRJDIR)
+    LAB1DIR = cfg_dir.get('raw-linguistics', path.join(DATDIR, 'lab1'))\
+              .replace('$', PRJDIR)
+    LAB2DIR = cfg_dir.get('trimmed-linguistics', path.join(DATDIR, 'lab2'))\
+              .replace('$', PRJDIR)
+    LAB3DIR = cfg_dir.get('normalized-linguistics', path.join(DATDIR, 'lab3'))\
+              .replace('$', PRJDIR)
+    ACO1DIR = cfg_dir.get('raw-acoustics', path.join(DATDIR, 'aco1'))\
+              .replace('$', PRJDIR)
+    ACO2DIR = cfg_dir.get('trimmed-acoustics', path.join(DATDIR, 'aco2'))\
+              .replace('$', PRJDIR)
+    ACO3DIR = cfg_dir.get('delta-acoustics', path.join(DATDIR, 'aco3'))\
+              .replace('$', PRJDIR)
+    TXTDIR = cfg_dir.get('transcripts', path.join(DATDIR, 'txt'))\
+             .replace('$', PRJDIR)
+    STTDIR = cfg_dir.get('statistics', path.join(DATDIR, 'stats'))\
+             .replace('$', PRJDIR)
+    TRNDIR = cfg_dir.get('training', path.join(DATDIR, 'training'))\
+             .replace('$', PRJDIR)
+    VALDIR = cfg_dir.get('validation', path.join(DATDIR, 'validation'))\
+             .replace('$', PRJDIR)
+    TSTDIR = cfg_dir.get('testing', path.join(DATDIR, 'training'))\
+             .replace('$', PRJDIR)
+    SYNDIR = cfg_dir.get('synthesized', path.join(DATDIR, 'synthesized'))\
+             .replace('$', PRJDIR)
 
-    global STDOUT = cfg_log.get('redirect-stdout', '&1')
-    global STDOUT = sys.stdout if STDOUT == '&1' else \
-                    sys.stderr if STDOUT == '&2' else \
-                    open(STDOUT, 'w')
-    global STDERR = cfg_log.get('redirect-stderr', '&2')
-    global STDERR = sys.stdout if STDERR == '&1' else \
-                    sys.stderr if STDERR == '&2' else \
-                    open(STDERR, 'w')
-    global VERBOSE = cfg_log.get('verbose', True)
+    STDOUT = cfg_log.get('redirect-stdout', '&1')
+    STDOUT = sys.stdout if STDOUT == '&1' else \
+             sys.stderr if STDOUT == '&2' else \
+             open(STDOUT, 'w')
 
+    STDERR = cfg_log.get('redirect-stderr', '&2')
+    STDERR = sys.stdout if STDERR == '&1' else \
+             sys.stderr if STDERR == '&2' else \
+             open(STDERR, 'w')
+
+    VERBOSE = cfg_log.get('verbose', True)
 
 def print1(*args, **kwargs):
     print(*args, file=STDOUT, **kwargs)
