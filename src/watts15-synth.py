9aa2c3c4420be7bb0b5322751295447099ba8961
kurt <s1788674@ed.ac.uk>
2018-06-08 04:57:00 +0100

    variance scaling

--- src/watts15-synth.py
+++ src/watts15-synth.py
@@ -78,9 +78,8 @@ if __name__ == '__main__':
             x = np.concatenate(output)
 
             # Variance scaling
-            u_x, s_x = np.mean(x, axis=0), np.std(x, axis=0)
-            # print2(u_x[ax.LF0], s_x[ax.LF0])
-            x = np.add(np.divide(np.subtract(x, u_x), s_x), u_x)
+            u_x, s_x = np.mean(x[:,ax.AX_LEN], axis=0), np.std(x[:,ax.AX_LEN], axis=0)
+            x[:,ax.AX_LEN] = np.add(np.divide(np.subtract(x[:,ax.AX_LEN], u_x), s_x), u_x)
 
             x = ax.destandardize(x, mean, stddev)
 
